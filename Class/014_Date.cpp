9194dc960d5dc3faebb8623a48f23d362370af67
Syed Ikramullah <154834130+syed-ikramullah@users.noreply.github.com>
2024-11-09 18:43:55 +0500

    014_Date.cpp

--- Class/014_Date.cpp
+++ Class/014_Date.cpp
@@ -1,6 +1,9 @@
 /*
-Write a C++ program to implement a class called Date that has private member variables for day, month, and year.
-Include member functions to set and get these variables, as well as to validate if the date is valid.
+Task 2:
+Create a class named as Date which has day, month, year as private attributes.
+Write a program to create two objects of date in main(). In one you have to ask user to enter his birthDate and in other the currentDate.
+Display the birthdate and currentDate of the user by display function. And then calculate age of the user from the objects and print it.
+
 */
 
 
@@ -16,45 +19,69 @@ private:
 	int year;
 
 public:
-	void display();
+	void display()const;
+
+	void setday(int D);
+	void setmonth(int M);
+	void setyear(int Y);
+	int getday()const;
+	int getmonth()const;
+	int getyear()const;
+
 	Date();
 	Date(int d, int m, int y);
-	Date(const Date& obj);
-	bool isleapyear();
-	bool isValid();
+	bool isleapyear()const;
+	bool isValid()const;
 };
-
                                       // .cpp file
 
 #include "Date.h"
 
-void Date::display() {
-	if (!isValid()) {
-		cout << endl << "Enter correct date and month!";
-	}
-	else {
+void Date::display() const{
+	if (isValid()) {
 		cout << "day: " << day << endl;
 		cout << "month: " << month << endl;
 		cout << "year: " << year << endl;
 		cout << endl << day << "/" << month << "/" << year << endl;
 	}
+	else {
+		cout << endl << "Enter the correct date and month!";
+	}
 }
+
+void Date::setday(int D) {
+	day = D;
+}
+void Date::setmonth(int M) {
+	month = M;
+}
+void Date::setyear(int Y) {
+	year = Y;
+}
+int Date::getday()const {
+	return day;
+}
+int Date::getmonth()const {
+	return month;
+}
+int Date::getyear()const {
+	return year;
+}
+
+
 Date::Date() {
 	day = 0;
 	month = 0;
 	year = 0;
 }
+
 Date::Date(int d, int m, int y) {
 	day = d;
 	month = m;
 	year = y;
 }
-Date::Date(const Date& obj) {
-	day = obj.day;
-	month = obj.month;
-	year = obj.year;
-}
-bool Date::isleapyear() {
+
+bool Date::isleapyear() const{
 	if (year % 4 == 0) {
 		return true;
 	}
@@ -62,7 +89,7 @@ bool Date::isleapyear() {
 		return false;
 	}
 }
-bool Date::isValid() {
+bool Date::isValid() const{
 	if (isleapyear() && (month == 2) && (day <= 29)) {
 		return true;
 	}
@@ -80,27 +107,57 @@ bool Date::isValid() {
 	}
 }
 
-
                                       // main.cpp
 
 
 #include "Date.h"
-
+void CalculateAge(const Date& BirthDate, const Date& CurrDate);
 int main() {
 
-	int d, m, y;
-	cout << "Enter day: ";
-	cin >> d;
-	cout << "Enter month: ";
-	cin >> m;
-	cout << "Enter year: ";
-	cin >> y;
+	int DayOfBirth, MonthOfBirth, YearOfBirth;
+	cout << "Enter your date of birth: ";
+	cin >> DayOfBirth;
+	cout << "Enter your month of birth: ";
+	cin >> MonthOfBirth;
+	cout << "Enter your year of birth: ";
+	cin >> YearOfBirth;
 
-	Date D(d, m, y);
+	Date BirthDate(DayOfBirth, MonthOfBirth, YearOfBirth);
 
-	D.display();
+
+	int CurrDay, CurrMonth, CurrYear;
+	cout << "Enter Current day: ";
+	cin >> CurrDay;
+	cout << "Enter Current month: ";
+	cin >> CurrMonth;
+	cout << "Enter Current year: ";
+	cin >> CurrYear;
+
+	Date CurrentDate(CurrDay, CurrMonth, CurrYear);
+
+	cout << endl << "Birth date: " << endl;
+	BirthDate.display();
+	cout << endl << endl << "Current date: " << endl;
+	CurrentDate.display();
+
+	CalculateAge(BirthDate, CurrentDate);
 
 	system("pause");
 	return 0;
 }
 
+void CalculateAge(const Date& BirthDate, const Date& CurrDate) {
+	int age = CurrDate.getyear() - BirthDate.getyear();
+	int month = CurrDate.getmonth() - BirthDate.getmonth();
+	if (month < 0) {
+		age--;
+	}
+	
+	int days = CurrDate.getday() - BirthDate.getday();
+	if (days < 0) {
+		month--;
+	}
+
+	cout << "Age of the user is " << age << " years, " << month << " months, and " << days << " days." << endl;
+
+}
